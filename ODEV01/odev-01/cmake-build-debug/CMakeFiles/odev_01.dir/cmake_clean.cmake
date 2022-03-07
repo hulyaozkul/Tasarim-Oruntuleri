a42e2988316c99be99fef9b29af0062f84fe1119
hulyaozkul <150202024@kocaeli.edu.tr>
2022-03-07 16:43:52 +0300

    added odev01

---
+++ ODEV01/odev-01/cmake-build-debug/CMakeFiles/odev_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/odev_01.dir/main.cpp.obj"
+  "libodev_01.dll.a"
+  "odev_01.exe"
+  "odev_01.exe.manifest"
+  "odev_01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/odev_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
